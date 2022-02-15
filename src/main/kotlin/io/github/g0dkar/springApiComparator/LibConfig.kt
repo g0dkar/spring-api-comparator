f730a7a7f2e745abaf6ce1578441eb9a0af6525e
Rafael Lins <rafael.lins@glovoapp.com>
2022-02-14 21:45:20 -0300

    Starting development

--- src/main/kotlin/io/github/g0dkar/springApiComparator/LibConfig.kt
+++ src/main/kotlin/io/github/g0dkar/springApiComparator/LibConfig.kt
@@ -3,7 +3,6 @@ package io.github.g0dkar.springApiComparator
 import io.github.g0dkar.springApiComparator.config.Configs
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
-import org.apache.hc.client5.http.impl.classic.HttpClients
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
@@ -14,37 +13,16 @@ import org.springframework.context.annotation.Configuration
 @ConfigurationPropertiesScan("io.github.g0dkar.springApiComparator.config")
 class LibConfig {
     @Bean
-    fun apiComparatorHttpClient(configs: Configs): CloseableHttpClient = HttpClients.createSystem()
+    fun apiComparatorHttpClient(
+        configs: Configs,
+        apiComparatorHttpClientBuilder: HttpClientBuilder
+    ): CloseableHttpClient = apiComparatorHttpClientBuilder.build()
 
-    fun httpClientBuilder(): HttpClientBuilder =
+    @Bean
+    fun apiComparatorHttpClientBuilder(): HttpClientBuilder =
         HttpClientBuilder.create()
             .apply {
                 setUserAgent("userAgent")
             }
             .useSystemProperties()
-
-    // fun httpClientBuilder(): HttpClientBuilder? {
-    //     val clientBuilder: HttpClientBuilder = HttpClientBuilder.create()
-    //     clientBuilder.setRequestExecutor(HttpRequestExecutorChain(chainableHttpRequestExecutors))
-    //     clientBuilder.setConnectionManager(connectionManager)
-    //     clientBuilder.setDefaultAuthSchemeRegistry(authSchemeRegistry)
-    //     clientBuilder.setDefaultCredentialsProvider(credentialsProvider)
-    //     clientBuilder.setDefaultRequestConfig(defaultRequestConfig)
-    //     clientBuilder.setTargetAuthenticationStrategy(CookieProcessingTargetAuthenticationStrategy.INSTANCE)
-    //     clientBuilder.setConnectionReuseStrategy(DefaultConnectionReuseStrategy.INSTANCE)
-    //     clientBuilder.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
-    //     val userAgent: String = config.getGlobalConfiguration(ConfigurationConstants.USER_AGENT)
-    //     clientBuilder.setUserAgent(userAgent)
-    //     requestInterceptors.forEach(clientBuilder::addInterceptorLast)
-    //     responseInterceptors.forEach(clientBuilder::addInterceptorLast)
-    //     val cookieStore: CookieStore = cookieStoreProvider.getIfAvailable()
-    //     if (config.isCookieManagementDisabled() || cookieStore == null) {
-    //         clientBuilder.disableCookieManagement()
-    //     } else {
-    //         log.info("Using cookie store {}", cookieStore)
-    //         clientBuilder.setDefaultCookieStore(cookieStore)
-    //     }
-    //     clientBuilder.setRoutePlanner(routePlanner)
-    //     return clientBuilder
-    // }
 }
